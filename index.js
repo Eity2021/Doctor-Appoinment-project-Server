8265a08aec96dd0e62f44c7cb24103d0761822dd
Eity2021 <Eity2021akter@gmail.com>
2022-09-29 05:31:52 +0600

    delete API

--- index.js
+++ index.js
@@ -140,16 +140,22 @@ async function run() {
       res.send(result);
     });
     //doctor get
-   
-    app.get('/doctor' , verifyJWT,verifyAdmin, async(req,res) =>{
+
+    app.get("/doctor", verifyJWT, verifyAdmin, async (req, res) => {
       const doctor = await doctorCollection.find().toArray();
       res.send(doctor);
-    })
+    });
     //doctors post
 
     app.post("/doctor", verifyJWT, verifyAdmin, async (req, res) => {
       const doctor = req.body;
       const result = await doctorCollection.insertOne(doctor);
+      res.send(result);
+    });
+    app.delete("/doctor/:email", verifyJWT, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await doctorCollection.deleteOne(filter);
       res.send(result);
     });
 
